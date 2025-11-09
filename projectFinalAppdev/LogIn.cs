65c605f0002ff8e0971b74271bbd895cc26120f8
FranciscoNathan <FranciscoNathan@DESKTOP-LFVF0K5>
2025-11-09 14:47:35 +0800

    Add project files.

---
+++ projectFinalAppdev/LogIn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectFinalAppdev
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+            this.Text = string.Empty;
+            this.ControlBox = false;
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
